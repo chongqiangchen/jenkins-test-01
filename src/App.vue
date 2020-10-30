a9a551f9fa205d67cd7031be12ac5a53334b4759
chong <chongqiangchen@gmail.com>
2020-10-30 16:17:09 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
+  123131
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3.0 + Vite" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
